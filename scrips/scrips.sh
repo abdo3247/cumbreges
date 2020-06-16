1a74daae7c40e1660c7d3708786e06453c83d06c
Mario Osvaldo Nuñez <marionumza@gmail.com>
2020-06-16 20:21:33 -0300

    1
    1

--- scrips/scrips.sh
+++ scrips/scrips.sh
@@ -1,8 +1,10 @@
 #archivos supervisor
 #fijate como crear un enlace simbólico
 cd ~/server/clientes/cumbreges/supervisor
-cp *.* /etc/supervisor/conf.d
+ln -s /home/cumbreges13/server/clientes/cumbreges/supervisor/prod_cumbreges13.conf /etc/supervisor/conf.d/prod_cumbreges13.conf
+ln -s /home/cumbreges13/server/clientes/cumbreges/supervisor/test_cumbreges13.conf /etc/supervisor/conf.d/test_cumbreges13.conf
 #copia configuracion de ribbon
 cp ribbon.css ~/server/sources/OCA-web/web_environment_ribbon/static/src/css/
+
 #Borrar mercadopago de adhoc
 rm -rf ~/server/sources/ingadhoc-website/payment_mercadopago
